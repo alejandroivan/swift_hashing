9dfe87fa13524490a1755c22126b71f08bcb0e6d
Alejandro Melo Domínguez <alejandroivan@icloud.com>
2019-10-24 18:36:12 -0300

    First implementation.

---
+++ Hash.swift
@@ -0,0 +1,76 @@
+import CommonCrypto
+import Foundation
+
+public class Hash {
+    /// An enumeration specifying the currently supported hashing algorithms.
+    ///
+    /// - sha256: Represents a SHA256 hashing algorithm.
+    /// - sha512: Represents a SHA512 hashing algorithm.
+    public enum HashType {
+        case sha256
+        case sha512
+    }
+
+    /// A typealias representing the closure types for the hashing funciton.
+    private typealias HashFunction = (UnsafeRawPointer?, CC_LONG, UnsafeMutablePointer<UInt8>?) -> UnsafeMutablePointer<UInt8>?
+
+    // MARK: - Properties
+
+    /// The variant to use for hashing.
+    public var variant: HashType
+
+    /// The length of the digest for the particular variant chosen, taken from CommonCrypto.
+    public var digestLength: Int32 {
+        switch variant {
+        case .sha256:
+            return CC_SHA256_DIGEST_LENGTH
+        case .sha512:
+            return CC_SHA512_DIGEST_LENGTH
+        }
+    }
+
+    /// Chooses a hashing function from CommonCrypto, based on the variant specified in the "variant" instance variable.
+    private var hashFunction: HashFunction {
+        switch variant {
+        case .sha256:
+            return CC_SHA256
+        case .sha512:
+            return CC_SHA512
+        }
+    }
+
+    // MARK: - Initialization
+
+    init(variant: HashType) {
+        self.variant = variant
+    }
+
+    /// Calculates the hashed bytes for the incoming Data.
+    ///
+    /// - Parameter data: A Data object representing the original object from which the hash is calculated.
+    /// - Returns: An byte array representing the hashed data.
+    public func bytes(for data: Data) -> [UInt8] {
+        var digest = [UInt8](
+            repeating: 0,
+            count: Int(digestLength)
+        )
+
+        data.withUnsafeBytes {
+            _ = hashFunction(
+                $0.baseAddress,
+                CC_LONG(data.count),
+                &digest
+            )
+        }
+
+        return digest
+    }
+
+    /// Transforms the input hashed bytes to a String representation.
+    ///
+    /// - Parameter bytes: A byte array representing hashed data.
+    /// - Returns: The byte array translated to a String.
+    public func string(from bytes: [UInt8]) -> String {
+        return bytes.map { String(format: "%02hhx", $0) }.joined()
+    }
+}
---
+++ String+Hash.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public extension String {
+    var sha256: String { return hash(for: .sha256) }
+    var sha512: String { return hash(for: .sha512) }
+
+    private func hash(for variant: Hash.HashType) {
+        // Force-unwrapping should never fail for valid UTF-8 strings
+        let data = self.data(using: .utf8)!
+        let hash = Hash(variant: variant)
+        let bytes = hash.bytes(for: data)
+        return hash.string(from: bytes)
+    }
+}
